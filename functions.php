44bd8f897bb878cc400f668d6ed949aa46038d3e
TQuang <quangt300497@gmail.com>
2023-03-24 19:34:45 +0700

    fix paginate

--- functions.php
+++ functions.php
@@ -225,8 +225,9 @@ if(!function_exists('azonow_pagination')){
         }?>
         <div class="wp-pagenavi">
             <?php if($current_page <= 4):?>
-                <?php for($i=1;$i<=$number_display;$i++):?>
-                    <?php if($i <= $total_page):?>
+            
+                <?php for($i=1;$i<=$total_page;$i++):?>
+                    <?php if($i <= $number_display):?>
                         <?php if($i<=6):?>
                             <?php if($current_page == $i):?>
                                 <span class="current"><?php echo $i?></span>
@@ -241,61 +242,59 @@ if(!function_exists('azonow_pagination')){
                             <?php if($i==$number_display-1):?>
                                 <span class="extend">...</span>
                             <?php elseif($i==$number_display):?>
-                                <a class="last" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>">&gt;</a>
+                                <a class="last" href="<?php echo $id_object_active_url ?>page/<?php echo ($total_page ) ?>">&gt;</a>
                             <?php endif?>
                         <?php else:?>
                             <?php if($i==$number_display):?>
-                                <a class="last" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>">&gt;</a>
+                                <a class="last" href="<?php echo $id_object_active_url ?>page/<?php echo ($total_page ) ?>">&gt;</a>
                             <?php endif?>
                         <?php endif;?>
                     <?php endif;?>
                 <?php endfor;?>
             <?php elseif($total_page - $current_page > 3):?>
-            <?php for($i=1;$i<=$number_display;$i++):?>
+                
+            <?php for($i=1;$i<=$total_page;$i++):?>
                 <?php if($i<=2):?>
                     <?php if($i==1):?>
                         <a class="first" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>">&lt;</a>
                     <?php else:?>
                         <span class="extend">...</span>
                     <?php endif?>
-                <?php endif;?>
-                <?php if($i== $current_page - 1):?>
-                    <a class="page smaller" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>"><?php echo $i?></a>
-                <?php endif;?>
-                <?php if($i==$current_page):?>
-                    <span class="current"><?php echo $i?></span>
-                <?php endif;?>
-                <?php if($i== $current_page + 1):?>
-                    <a class="page larger" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>"><?php echo $i?></a>
-                <?php endif;?>
-                <?php if($i== $current_page + 2):?>
-                    <a class="page larger" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>"><?php echo $i?></a>
-                <?php endif;?>
-                <?php if($i==$number_display-1):?>
+                <?php elseif($i==3):?>
+                    <a class="page smaller" href="<?php echo $id_object_active_url ?>page/<?php echo $current_page-1 ?>"><?php echo $current_page-1?></a>
+                
+                <?php elseif($i==4):?>
+                    <span class="current"><?php echo $current_page?></span>
+                
+                <?php elseif($i== 5):?>
+                    <a class="page larger" href="<?php echo $id_object_active_url ?>page/<?php echo $current_page+1 ?>"><?php echo $current_page+1?></a>
+                
+                <?php elseif($i== 6):?>
+                    <a class="page larger" href="<?php echo $id_object_active_url ?>page/<?php echo $current_page+2 ?>"><?php echo $current_page+2?></a>
+                
+                <?php elseif($i==7):?>
                     <span class="extend">...</span>
-                <?php elseif($i==$number_display):?>
-                    <a class="last" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>">&gt;</a>
+                <?php elseif($i==8):?>
+                    <a class="last" href="<?php echo $id_object_active_url ?>page/<?php echo $total_page ?>">&gt;</a>
                 <?php endif?>
             <?php endfor;?>
             <?php else:?>
-                <?php for($i=1;$i<=$number_display;$i++):?>
+                
+                <?php for($i=1;$i<=$total_page;$i++):?>
                     <?php if($i<=2):?>
                         <?php if($i==1):?>
                             <a class="first" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>">&lt;</a>
                         <?php else:?>
                             <span class="extend">...</span>
                         <?php endif?>
-                    <?php endif;?>
-                    <?php if($total_page - $i < 6):?>
-                        <?php if($current_page == $i):?>
-                                <span class="current"><?php echo $i?></span>
+                    <?php elseif($number_display- $i >=0):?>
+                        <?php if(($total_page - ($number_display - $i)) == $current_page):?>
+                            <span class="current"><?php echo $current_page?></span>
+                        <?php elseif(($total_page - ($number_display - $i))>$current_page):?>
+                            <a class="page larger" href="<?php echo $id_object_active_url ?>page/<?php echo ($total_page - ($number_display - $i)) ?>"><?php echo ($total_page - ($number_display - $i))?></a>
                         <?php else:?>
-                                <?php if($current_page < $i):?>
-                                    <a class="page larger" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>"><?php echo $i?></a>
-                                <?php else:?>
-                                    <a class="page smaller" href="<?php echo $id_object_active_url ?>page/<?php echo $i ?>"><?php echo $i?></a>
-                                <?php endif;?>
-                    <?php endif;?>
+                            <a class="page smaller" href="<?php echo $id_object_active_url ?>page/<?php echo ($total_page - ($number_display - $i)) ?>"><?php echo ($total_page - ($number_display - $i))?></a>
+                        <?php endif?>
                     <?php endif;?>
                 <?php endfor;?>
             <?php endif;?>
@@ -321,10 +320,11 @@ if(!function_exists('azonow_category_list')){
         <?php
             $get_author_id = get_the_author_meta('ID');
             $get_author_gravatar = get_avatar_url($get_author_id, array('size' => 450));
+            $get_author_url = get_author_posts_url($get_author_id);
         ?>
         <?php if(have_posts()):?>
             <?php foreach($posts as $key =>$post):?>
-                <div id="post-<?php echo $post->ID ?>" class="post-holder clearfix post-<?php echo $post->ID ?> post type-post status-publish format-standard has-post-thumbnail hentry category-marketing <?php echo (int)$key%2 != 0 ? "odd" : "even"  ?>"><div class="container"><div class="row"><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><header class="post-header"><div class="post-category"><a href="<?php echo $id_object_active_url ?>" rel="tag"><?php echo $id_object_active_name?></a></div><h3> <a href="<?php echo get_permalink( $post->ID ); ?>" rel="bookmark" title="Permanent Link to <?php echo $post->post_title ?>"><?php echo $post->post_title ?></a></h3><div class="post-meta"> <span class="post-author vcard author" itemprop="author"> <span class="post-author-avatar"><img src="<?php echo $get_author_gravatar ?>" data-lazy-type="image" data-src="<?php echo $get_author_gravatar ?>" width="30" height="30" alt="<?php echo get_the_author() ?>" class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo lazy-loaded" style=""><noscript><img src="<?php echo $get_author_gravatar ?>" width="30" height="30" alt="<?php echo get_the_author() ?>" class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo" /></noscript></span> <a href="<?php get_the_author_link() ?>" title="Posts by <?php echo get_the_author() ?>" rel="author"> <span class="fn"><?php echo get_the_author() ?></span> </a> </span> <span class="post-date published updated" itemprop="datePublished"><?php echo get_the_date() ?></span></div></header></div></div></div></div>
+                <div id="post-<?php echo $post->ID ?>" class="post-holder clearfix post-<?php echo $post->ID ?> post type-post status-publish format-standard has-post-thumbnail hentry category-marketing <?php echo (int)$key%2 != 0 ? "odd" : "even"  ?>"><div class="container"><div class="row"><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><header class="post-header"><div class="post-category"><a href="<?php echo $id_object_active_url ?>" rel="tag"><?php echo $id_object_active_name?></a></div><h3> <a href="<?php echo get_permalink( $post->ID ); ?>" rel="bookmark" title="Permanent Link to <?php echo $post->post_title ?>"><?php echo $post->post_title ?></a></h3><div class="post-meta"> <span class="post-author vcard author" itemprop="author"> <span class="post-author-avatar"><img src="<?php echo $get_author_gravatar ?>" data-lazy-type="image" data-src="<?php echo $get_author_gravatar ?>" width="30" height="30" alt="<?php echo get_the_author() ?>" class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo lazy-loaded" style=""><noscript><img src="<?php echo $get_author_gravatar ?>" width="30" height="30" alt="<?php echo get_the_author() ?>" class="avatar avatar-30 wp-user-avatar wp-user-avatar-30 alignnone photo" /></noscript></span> <a href="<?php echo $get_author_url ?>" title="Posts by <?php echo get_the_author() ?>" rel="author"> <span class="fn"><?php echo get_the_author() ?></span> </a> </span> <span class="post-date published updated" itemprop="datePublished"><?php echo get_the_date() ?></span></div></header></div></div></div></div>
                 
             <?php endforeach;?>
      
