4f44f5b4ed2e4c8ac549d520405506299c72162c
Lamdu125 <thanhlam5797@gmail.com>
2023-03-28 12:58:41 +0700

    m

--- includes/feature-blog/add-scripts.php
+++ includes/feature-blog/add-scripts.php
@@ -1,8 +1,10 @@
 <?php
 
 function azonow_add_blog_scripts( $hook ) {
-    wp_enqueue_media();
-    wp_enqueue_style( 'blog-style', get_stylesheet_directory_uri() . '/assets/css/blog-style.css' );
+    if(is_page()&&!is_admin()){
+        wp_enqueue_media();
+        wp_enqueue_style( 'blog-style', get_stylesheet_directory_uri() . '/assets/css/blog-style.css' );
+   }
 }
 add_action( 'admin_enqueue_scripts', 'azonow_add_blog_scripts');
 
